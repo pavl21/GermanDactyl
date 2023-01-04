d0c1c96a877c652a489b0da2331a64adf04bd711
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-04 03:16:29 +0100

    The startPatching.sh now applies all previous patches

--- scripts/startPatching.sh
+++ scripts/startPatching.sh
@@ -10,6 +10,16 @@ wget -q -O - https://github.com/pterodactyl/panel/releases/latest/download/panel
 # Switch to patch mode
 git reset --hard HEAD
 git checkout -b patches
+
+# Apply previous patch
+patch_files=$(ls -1 patches/*.patch 2>/dev/null | wc -l)
+if [ "$patch_files" != 0 ]; then
+    patch=$(ls -t patches/*.patch | head -1)
+    git apply --ignore-whitespace --ignore-space-change -C1 --inaccurate-eof --apply --allow-empty --reject "$patch"
+    find . -name \*.rej -print0 | xargs rm
+    find . -name \*.orig -print0 | xargs rm
+fi
+
 git add .
 git commit -m "base"
 git tag base
