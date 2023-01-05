6ccd80ae98ad8d27b8dad1291e0531a55009cbb7
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-05 01:26:08 +0100

    Updated the startPatching.sh script

--- scripts/startPatching.sh
+++ scripts/startPatching.sh
@@ -16,17 +16,18 @@ patch_files=$(ls -1 patches/*.patch 2>/dev/null | wc -l)
 if [ "$patch_files" != 0 ]; then
     patch=$(ls -t patches/*.patch | head -1)
     git apply --ignore-whitespace --ignore-space-change -C1 --inaccurate-eof --apply --allow-empty --reject "$patch"
-    echo -e "\033[1;31m✗ Folgende Dateien konnten nicht gepatcht werden und brauchen manuelle Übersetzungen:\033[0;39m"
-    found_files=no
+
+    rejected_files=$(find . \( -name "*.rej" -o -name "*.orig" \) | wc -l)
+    if [ "$rejected_files" == 0 ]; then
+        echo -e "\033[1;32m✓ Beim Patch-Vorgang traten keine Fehler auf.\033[0;39m"
+    else
+        echo -e "\033[1;31m✗ Folgende Dateien konnten nicht gepatcht werden und brauchen manuelle Übersetzungen:\033[0;39m"
+    fi
+
     find . \( -name "*.rej" -o -name "*.orig" \) | while read -r file; do
-        found_files=yes
         file_pretty=$(echo "$file" | cut -c 3-)
         echo -e "\033[1;31m✗ ${file_pretty%.*}\033[0;39m"
         rm "$file"
     done
-
-    if [ "$found_files" == "no" ]; then
-        echo -e "\033[1;31m✗ Alle Patches wurden sauber angewendet. Keine manuellen Eingriffe erforderlich."
-    fi
 
 fi
