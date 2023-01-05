2e2a28a10bde0963f78040bd5b59388c607f97d0
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-05 16:42:53 +0100

    Updated the createPatch.sh

--- scripts/createPatch.sh
+++ scripts/createPatch.sh
@@ -5,7 +5,9 @@
 version=$(git ls-remote --refs --sort="version:refname" --tags https://github.com/pterodactyl/panel/ | cut -d/ -f3- | tail -n1)
 
 # Build a patch
-git format-patch base..HEAD --stdout >> "patches/$version.patch"
+git add resources app routes database
+git commit -m "Translations $version"
+git format-patch base..HEAD --stdout > "patches/$version.patch"
 git checkout main
 git tag -d base
 git branch -D patches
