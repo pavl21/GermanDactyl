23e569bc9bb27b95181343c5466d3bccf55d67ec
Mathias Wagner <germannewsmaker@gmail.com>
2023-01-16 20:07:47 +0100

    Updated the createPatch.sh

--- scripts/createPatch.sh
+++ scripts/createPatch.sh
@@ -8,6 +8,7 @@ version=$(git ls-remote --refs --sort="version:refname" --tags https://github.co
 git add resources app routes database public
 git commit -m "Translations $version"
 git format-patch base..HEAD --stdout > "patches/$version.patch"
+rm -r public
 git add .
 git commit -m "Revert changes"
 git checkout main
